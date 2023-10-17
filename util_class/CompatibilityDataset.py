f97b41af836875ca4cdcbb3aa2c14f27484fe6e3
Yuta123456 <yuuta09090530@icloud.com>
2023-10-17 13:10:43 +0900

    2023-10-17

---
+++ util_class/CompatibilityDataset.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+from torchvision import transforms
+from transformers import AutoTokenizer, AutoModel
+import numpy as np
+from PIL import Image
+
+
+class CompatibilityDataset(Dataset):
+    def __init__(self, positive_file, negative_file, n=10000):
+        # TODO: ? サンプリング
+        self.postive_annotations = pd.read_csv(positive_file, header=None).sample(
+            n=n, random_state=1
+        )
+        self.negative_annotations = pd.read_csv(negative_file, header=None).sample(
+            n=n * 16, random_state=1
+        )
+
+        self.data = pd.concat(
+            [self.postive_annotations, self.negative_annotations], axis=0
+        )
+        # print(self.data[:5])
+        self.transform = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image_1 = self.to_image(self.data.iloc[idx, 0])
+        image_2 = self.to_image(self.data.iloc[idx, 2])
+
+        # lstmに入れる形にする
+        caption_1 = self.data.iloc[idx, 1]
+        caption_2 = self.data.iloc[idx, 3]
+
+        label = self.data.iloc[idx, 4]
+        return image_1, caption_1, image_2, caption_2, label
+
+    def to_image(self, image_path):
+        image = Image.open(image_path)
+        if image.mode != "RGB":
+            image = image.convert("L")
+            image = Image.merge("RGB", [image] * 3)
+        image = self.transform(image)
+        return image
